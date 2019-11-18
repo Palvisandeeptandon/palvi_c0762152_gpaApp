d60116b93106bf79e1d42e5ddc73b21a92fc086e
palvi <pgander007@gmail.com>
2019-11-18 16:36:22 -0500

    done

---
+++ Palvi_c0762152_gpaAppTests/Palvi_c0762152_gpaAppTests.swift
@@ -0,0 +1,34 @@
+//
+//  Palvi_c0762152_gpaAppTests.swift
+//  Palvi_c0762152_gpaAppTests
+//
+//  Created by Admin on 17/11/19.
+//  Copyright © 2019 Palvi. All rights reserved.
+//
+
+import XCTest
+@testable import Palvi_c0762152_gpaApp
+
+class Palvi_c0762152_gpaAppTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
